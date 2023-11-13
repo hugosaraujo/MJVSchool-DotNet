2804ea97df2952b337b1d7b28706fb80658301b5
hugo de araujo <hugo.saraujo@tutanota.com>
2023-11-13 11:14:42 -0300

    Testando o funcionamento das structs.

--- TestesDeCodigo/Program.cs
+++ TestesDeCodigo/Program.cs
@@ -1,5 +1,26 @@
-﻿Console.WriteLine("Projeto apenas para testar os trechos de código passados em aula");
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Projeto apenas para testar os trechos de código passados em aula");
+
+Pessoa p = new Pessoa();
+p.Nome = "Hugo";
+p.Idade = 29;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Hugo";
+p1.Idade = 29;
+
+bool comparacaoNomes = p.Nome == p1.Nome; 
+
+Console.WriteLine(comparacaoNomes);
 
 
 Console.WriteLine("Pressione qualquer tecla para fechar a aplicação");
 Console.ReadKey();
+
+
+struct Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
