61ef355ed88c297b3e6229f55ac585e0b852b5d5
hugo de araujo <hugo.saraujo@tutanota.com>
2023-11-13 11:02:23 -0300

    Escrita de console com a idade em que a data da morte do usuário.

--- Pressagio/Program.cs
+++ Pressagio/Program.cs
@@ -1,14 +1,10 @@
 ﻿//Exercício - 1; 
-//Campo para o usuário digitar o nome; 
-//Campo para digitação de data de nascimento; 
 //Mostrar para o usuário o dia em que ele estiver vivo 
 //Criação de validação de data de nascimento;
     //criação de um tratamento de exceção para tratar System.FormatException
 //Criação dos outros projetos do curso da MJV
 /*
     -> criação de projeto de loteria para sortear jogos
-    -> criação de projeto de serialização de Json
-    -> teste de webapp, começo dos estudos de ASP.MVC
 */
 
 using System.Runtime.InteropServices.ComTypes;
